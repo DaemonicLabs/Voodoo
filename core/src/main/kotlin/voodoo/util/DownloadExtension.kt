af1873eb00d6948e2e8ba1d3905d7573a890b2a0
Nikky Ai <root@nikky.moe>
2018-04-09 21:35:30 +0200

    for now we accept that windows might prevent files from being upgraded

--- core/src/main/kotlin/voodoo/util/DownloadExtension.kt
+++ core/src/main/kotlin/voodoo/util/DownloadExtension.kt
@@ -40,10 +40,13 @@ fun File.download(url: String, cacheDir: File, useragent: String = downloader.us
             }
         }
     }
+    
     try {
         cacheFile.copyTo(this, overwrite = true)
     }catch(e: FileAlreadyExistsException) {
-        logger.error("failed to copy file $cacheFile to $this .. trying again")
-        cacheFile.copyTo(this, overwrite = true)
+        val fileIsLocked = !this.renameTo(this)
+        logger.error("failed to copy file $cacheFile to $this .. file is locked ? $fileIsLocked")
+        if(!fileIsLocked)
+            cacheFile.copyTo(this, overwrite = true)
     }
 }
