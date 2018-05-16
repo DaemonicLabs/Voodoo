2caba0b2e15487a82de6d475e941987a0ade8e60
nikky <niklasbloedorn@gmx.de>
2018-05-16 19:29:59 +0200

    add murmur hash checking and changelogs to archiver

--- archiver/src/main/kotlin/archiver/Main.kt
+++ archiver/src/main/kotlin/archiver/Main.kt
@@ -6,8 +6,6 @@ package archiver
  * @version 1.0
  */
 
-import archiver.curse.CurseUtil
-import archiver.curse.CurseUtil.META_URL
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
 import com.xenomachina.argparser.ArgParser
@@ -15,6 +13,8 @@ import com.xenomachina.argparser.InvalidArgumentException
 import com.xenomachina.argparser.default
 import com.xenomachina.argparser.mainBody
 import mu.KotlinLogging
+import voodoo.curse.CurseUtil
+import voodoo.curse.Murmur2Hash
 import java.io.File
 import java.util.concurrent.Executors
 
@@ -26,6 +26,9 @@ fun main(vararg args: String) = mainBody {
 
     arguments.run {
 
+        val outputDir = File(targetArg ?: "output")
+        logger.info("output directory: $outputDir")
+
         val feed = when (mode) {
             FeedType.HOURLY -> {
                 CurseUtil.getFeed(true)
@@ -35,38 +38,83 @@ fun main(vararg args: String) = mainBody {
             }
         }
 
-        val outputDir = File(targetArg ?: "output")
-
         logger.info("${feed.count()} AddOns to be downloaded")
+        logger.info("starting $threadCount workers")
         val executor = Executors.newFixedThreadPool(threadCount)
         for (addOn in feed) {
             val worker = Runnable {
                 println("Getting AddOn ${addOn.id} ${addOn.name}")
                 val destDir = File(outputDir, addOn.id.toString())
-                val files = CurseUtil.getAllFilesForAddOn(addOn.id, META_URL)
+                val files = CurseUtil.getAllFilesForAddOn(addOn.id, CurseUtil.META_URL)
                 for (file in files) {
-                    val destFile = File(destDir, "${file.id}/${file.fileNameOnDisk}")
-                    val url = file.downloadURL
-                    logger.info("downloading {}", url)
-                    if (destFile.exists() && !destFile.isFile) destFile.delete()
-                    if (!destFile.exists()) {
-                        val (request, response, result) = url.httpGet().response()
-                        when (result) {
-                            is Result.Success -> {
-                                destFile.parentFile.mkdirs()
-                                destFile.writeBytes(result.value)
-                            }
-                            is Result.Failure -> {
-                                logger.error("invalid statusCode {} from {}", response.statusCode, url)
-                                logger.error("connection url: {}", request.url)
-                                logger.error("content: {}", result.component1())
-                                logger.error("error: {}", result.error.toString())
-                                throw Exception("broken download https://cursemeta.dries007.net/api/v2/direct/GetAddOnFile/${addOn.id}/${file.id}")
+                    var finishedFile = false
+                    var failCount = 0
+                    start@do {
+                        if(++failCount > 5) {
+                            throw Exception("failed addon: ${addOn.id} file: ${file.id}")
+                        }
+                        val destFile = File(destDir, "${file.id}/${file.fileNameOnDisk}")
+                        val url = file.downloadURL
+                        logger.info("downloading $url")
+                        if (destFile.exists() && !destFile.isFile) destFile.delete()
+                        var hash: Long
+                        if (destFile.exists()) {
+                            hash = Murmur2Hash.computeFileHash(destFile.absolutePath, true)
+                            if (hash != file.packageFingerprint) {
+                                logger.error { "hash of cached file does not match $hash != ${file.packageFingerprint}" }
+                                destFile.delete()
                             }
                         }
-                    } else {
-                        logger.info("skipping downloading ${file.fileName} (is cached)")
+
+                        if (!destFile.exists()) {
+                            val (request, response, result) = url.httpGet().response()
+                            when (result) {
+                                is Result.Success -> {
+                                    destFile.parentFile.mkdirs()
+                                    destFile.createNewFile()
+                                    destFile.writeBytes(result.value)
+
+                                    hash = Murmur2Hash.computeHash(result.value, true)
+                                    if (hash != file.packageFingerprint) {
+                                        logger.error("murmur mash mismatch $hash != ${file.packageFingerprint} addon: ${addOn.id} file: ${file.id}")
+                                        finishedFile = false
+                                        continue@start
+                                    }
+                                    finishedFile = true
+                                }
+                                is Result.Failure -> {
+                                    logger.error("invalid statusCode {} from {}", response.statusCode, url)
+                                    logger.error("connection url: {}", request.url)
+                                    logger.error("content: {}", result.component1())
+                                    logger.error("error: {}", result.error.toString())
+                                    logger.error("broken download https://cursemeta.dries007.net/api/v2/direct/GetAddOnFile/${addOn.id}/${file.id}")
+
+                                    finishedFile = false
+                                    continue@start
+                                }
+                            }
+                        } else {
+                            logger.info("skipping downloading ${file.fileName} (is cached)")
+                        }
+                    } while(!finishedFile)
+
+
+                    // changelog
+                    if (doChangelog) {
+                        val changelogFile = File(destDir, "${file.id}/changelog.txt")
+                        if (changelogFile.exists() && !changelogFile.isFile) changelogFile.delete()
+
+                        if (!changelogFile.exists()) {
+                            val changelog = CurseUtil.getFileChangelog(addOn.id, file.id, CurseUtil.META_URL)
+                            if (changelog != null) {
+                                changelogFile.createNewFile()
+                                changelogFile.writeText(changelog)
+                            }
+                        } else {
+                            logger.info("skipping downloading changelog for ${file.fileName} (is cached)")
+                        }
                     }
+
                 }
             }
             executor.execute(worker)
@@ -88,6 +136,10 @@ private class Arguments(parser: ArgParser) {
             "--hourly" to FeedType.HOURLY,
             "--complete" to FeedType.COMPLETE,
             help = "mode of operation").default(FeedType.COMPLETE)
+
+    val doChangelog by parser.flagging("-c", "--changelog",
+            help = "downloads changelogs")
+            .default(false)
 
     val threadCount by parser.storing("--threads",
             help = "number of cores") { toInt() }
