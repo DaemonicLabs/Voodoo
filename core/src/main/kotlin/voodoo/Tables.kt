d7a760ea0acfddf9f8bd3b61346ddf8b1f277b61
NikkyAI <root@nikky.moe>
2018-08-28 00:26:54 +0200

    Small tweaks to the modlist

--- core/src/main/kotlin/voodoo/Tables.kt
+++ core/src/main/kotlin/voodoo/Tables.kt
@@ -1,6 +1,8 @@
 package voodoo
+private val Any?.clean: String
+        get() = this.toString().replace("|", "\\|")
 
 fun <A, B> markdownTable(header: Pair<A, B>, content: List<Pair<A, B>>) =
-        "${header.first} | ${header.second}\n" +
+        "${header.first.clean} | ${header.second.clean}\n" +
                 "---|---\n" +
-               content.joinToString(separator = "\n") {  "${it.first} | ${it.second}" }
+               content.joinToString(separator = "\n") {  "${it.first.clean} | ${it.second.clean}" }
