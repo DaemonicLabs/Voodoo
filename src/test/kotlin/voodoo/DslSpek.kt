8e3eba90f5af2d1081092a5ef5e15c09f45e0fe2
NikkyAI <root@nikky.moe>
2018-09-09 19:37:30 +0200

    add simple DSL test

---
+++ src/test/kotlin/voodoo/DslSpek.kt
@@ -0,0 +1,82 @@
+package voodoo
+
+import kotlinx.coroutines.runBlocking
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import voodoo.builder.resolve
+import voodoo.data.Side
+import voodoo.data.curse.FileType
+import voodoo.data.flat.ModPack
+import voodoo.data.nested.NestedPack
+import voodoo.dsl.*
+import voodoo.importer.YamlImporter
+import voodoo.provider.CurseProviderThing
+import voodoo.provider.JenkinsProviderThing
+import java.io.*
+
+object DslSpek : Spek({
+    describe("load pack") {
+//        val rootFolder by memoized {
+//            File("run").resolve("test").resolve("build").absoluteFile.apply {
+//                deleteRecursively()
+//                mkdirs()
+//            }
+//        }
+
+//        beforeEachTest {
+//            fileForResource("/voodoo/dslSpek").copyRecursively(rootFolder)
+//        }
+
+        val nestedPack by memoized {
+            NestedPack(
+                    id = "some-id",
+                    version = "1.0",
+                    //TODO: type = File
+                    icon = "icon.png",
+                    authors = listOf("dude", "and", "friends"),
+                    //TODO: type = {recommended, latest} | buildnumber, make sealed class
+                    forge = "recommended",
+                    root = root(CurseProviderThing) {
+                        optionals = false
+                        releaseTypes = setOf(FileType.RELEASE, FileType.BETA)
+
+                        //TODO: use type URL ?
+                        metaUrl = "https://curse.nikky.moe"
+                        entries {
+                            id("botania") optionals false
+
+                            id("rf-tools") {
+                                optionals = false
+                            }
+
+                            entry(JenkinsProviderThing) {
+                                side = Side.SERVER
+                            }.entries {
+                                id("matterlink") job "elytra/matterlink/master"
+                                id("elytra/btfu/master")
+                            }
+
+//                    include("other.kts")
+                        }
+                    }
+            )
+        }
+
+        val modpack by memoized {
+            nestedPack.flatten()
+        }
+        val entries by memoized {
+            runBlocking(context = exceptionHandler) {
+                nestedPack.root.flatten(File(""))
+            }
+        }
+        it("simple test") {
+            println(nestedPack)
+            for (entry in entries) {
+                println(entry)
+            }
+            println(modpack)
+        }
+    }
+})
+
