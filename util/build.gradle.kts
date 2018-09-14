7c4cdbfad373e265e8f27879045497507b7a82cd
NikkyAI <root@nikky.moe>
2018-09-14 17:56:39 +0200

    manual memoizing of jenkins functions

--- util/build.gradle.kts
+++ util/build.gradle.kts
@@ -6,6 +6,7 @@ val jackson_version: String by project
 val kotlin_version: String by project
 val ktor_version: String by project
 val serialization_version: String by project
+//val kotlin_coroutines_version: String by project
 apply(from = rootProject.file("base.gradle.kts"))
 dependencies {
     compile(kotlin("reflect", kotlin_version))
@@ -18,16 +19,13 @@ dependencies {
 //    compile(group = "io.ktor", name ="ktor-client", version = ktor_version)
     compile(group = "io.ktor", name ="ktor-client-cio", version = ktor_version)
     compile(group = "io.ktor", name ="ktor-client-json-jvm", version = ktor_version)
-//    compile(group = "io.ktor", name ="ktor-client-kotlinx-serialization", version = ktor_version)
-//    compile(group = "io.ktor", name ="ktor-client-json", version = ktor_version)
-//    compile(group = "io.ktor", name ="ktor-client-jackson", version = ktor_version)
 
-//    compile(group = "io.ktor", name ="ktor-client-features", version = ktor_version)
     compile(group = "com.github.kittinunf.fuel", name = "fuel", version = fuel_version)
     compile(group = "com.github.kittinunf.fuel", name = "fuel-coroutines", version = fuel_version)
 //    compile(project(":fuel-coroutines"))
 
+//    compile(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-slf4j", version = kotlin_coroutines_version)
     compile(group = "io.github.microutils", name = "kotlin-logging", version = "1.6.10")
-//    compile(group = "ch.qos.logback", name = "logback-classic", version = "+")
-    compile(group = "org.slf4j", name = "slf4j-simple", version = "1.8.0-beta2")
+    compile(group = "ch.qos.logback", name = "logback-classic", version = "+")
+//    compile(group = "org.slf4j", name = "slf4j-simple", version = "1.8.0-beta2")
 }
