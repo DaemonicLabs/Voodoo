110feda1d99acc716cd2b0103b68782629190eaf
NikkyAi <root@nikky.moe>
2018-09-14 15:04:55 +0200

    fix test

--- src/test/kotlin/voodoo/DslSpek.kt
+++ src/test/kotlin/voodoo/DslSpek.kt
@@ -12,7 +12,7 @@ import java.io.*
 
 object DslSpek : Spek({
     describe("load pack") {
-//        val rootFolder by memoized {
+        //        val rootFolder by memoized {
 //            File("run").resolve("test").resolve("build").absoluteFile.apply {
 //                deleteRecursively()
 //                mkdirs()
@@ -25,36 +25,37 @@ object DslSpek : Spek({
 
         val nestedPack by memoized {
             NestedPack(
-                    id = "some-id",
-                    version = "1.0",
-                    //TODO: type = File
-                    icon = "icon.png",
-                    authors = listOf("dude", "and", "friends"),
-                    //TODO: type = {recommended, latest} | buildnumber, make sealed class
-                    forge = "recommended",
-                    root = rootEntry(CurseProvider) {
-                        optionals = false
-                        releaseTypes = setOf(FileType.RELEASE, FileType.BETA)
+                id = "some-id",
+                version = "1.0",
+                mcVersion = "1.12.2",
+                //TODO: type = File
+                icon = "icon.png",
+                authors = listOf("dude", "and", "friends"),
+                //TODO: type = {recommended, latest} | buildnumber, make sealed class
+                forge = "recommended",
+                root = rootEntry(CurseProvider) {
+                    optionals = false
+                    releaseTypes = setOf(FileType.RELEASE, FileType.BETA)
 
-                        //TODO: use type URL ?
-                        metaUrl = "https://curse.nikky.moe"
-                        entriesBlock {
-                            id("botania") optionals false
+                    //TODO: use type URL ?
+                    metaUrl = "https://curse.nikky.moe"
+                    entriesBlock {
+                        id("botania") optionals false
 
-                            id("rf-tools") {
-                                optionals = false
-                            }
+                        id("rf-tools") {
+                            optionals = false
+                        }
 
-                            entry(JenkinsProvider) {
-                                side = Side.SERVER
-                            }.entriesBlock {
-                                id("matterlink") job "elytra/matterlink/master"
-                                id("elytra/btfu/master")
-                            }
+                        entry(JenkinsProvider) {
+                            side = Side.SERVER
+                        }.entriesBlock {
+                            id("matterlink") job "elytra/matterlink/master"
+                            id("elytra/btfu/master")
+                        }
 
 //                    include("other.kts")
-                        }
                     }
+                }
             )
         }
 
