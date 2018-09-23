75ddf14e891f09175d0b5aefa85e2601b5be72ba
NikkyAi <root@nikky.moe>
2018-09-23 03:44:15 +0200

    update timeouts for curse client

--- core/src/main/kotlin/voodoo/curse/CurseClient.kt
+++ core/src/main/kotlin/voodoo/curse/CurseClient.kt
@@ -39,8 +39,8 @@ object CurseClient : KLogging() {
                 maxConnectionsPerRoute = 100 // Maximum number of requests for a specific endpoint route.
                 pipelineMaxSize = 20 // Max number of opened endpoints.
                 keepAliveTime = 5000 // Max number of milliseconds to keep each connection alive.
-                connectTimeout = 5000 // Number of milliseconds to wait trying to connect to the server.
-                connectRetryAttempts = 5 // Maximum number of attempts for retrying a connection.
+                connectTimeout = 2500 // Number of milliseconds to wait trying to connect to the server.
+                connectRetryAttempts = 3 // Maximum number of attempts for retrying a connection.
             }
         }
         defaultRequest {
