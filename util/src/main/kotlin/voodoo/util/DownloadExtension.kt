e59db93f1916edff825afbfdfeb4fed987d4969a
NikkyAi <root@nikky.moe>
2018-09-26 00:07:39 +0200

    use fuel to fix broken curse downloads

--- util/src/main/kotlin/voodoo/util/DownloadExtension.kt
+++ util/src/main/kotlin/voodoo/util/DownloadExtension.kt
@@ -1,5 +1,9 @@
 package voodoo.util
 
+import awaitByteArrayResponse
+import com.github.kittinunf.fuel.core.isStatusRedirection
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.features.defaultRequest
@@ -7,6 +11,7 @@ import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.response.HttpResponse
 import io.ktor.client.response.readBytes
+import kotlinx.io.IOException
 import mu.KLogger
 import mu.KLogging
 import voodoo.util.UtilConstants.VERSION
@@ -38,20 +43,42 @@ suspend fun File.download(
     logger: KLogger = Downloader.logger
 ) {
     val cacheFile = cacheDir.resolve(this.name)
+    val fixedUrl = url.encoded
     logger.info("downloading $url -> ${this@download}")
     logger.debug("cacheFile $cacheFile")
     if (cacheFile.exists() && !cacheFile.isFile) cacheFile.deleteRecursively()
 
     if (!cacheFile.exists() || !cacheFile.isFile) {
-        val response = Downloader.client.get<HttpResponse>(url.encoded)
-        val bytes: ByteArray = response.readBytes()
-        if(bytes.isEmpty()) {
-            logger.error("response status: ${response.status}")
-            throw IllegalStateException("content is empty: $response")
-        }
-        cacheDir.mkdirs()
-        cacheFile.parentFile.mkdirs()
-        cacheFile.writeBytes(bytes)
+        var nextUrl = url
+        do {
+            nextUrl = nextUrl.encoded
+            logger.info { nextUrl }
+            val (request, response, result) = nextUrl //.encode()
+                .httpGet().header("User-Agent" to Downloader.useragent)
+                .allowRedirects(false)
+                .awaitByteArrayResponse()
+            val isRedirect = when (result) {
+                is Result.Success -> {
+                    cacheDir.mkdirs()
+                    cacheFile.parentFile.mkdirs()
+                    cacheFile.writeBytes(result.value)
+                    false
+                }
+                is Result.Failure -> {
+                    if (response.isStatusRedirection) {
+                        nextUrl = response.headers["Location"]?.firstOrNull() ?:
+                            throw IllegalStateException("missing Location header")
+                        true
+                    } else {
+                        logger.error("invalid statusCode {} from {}", response.statusCode, fixedUrl)
+                        logger.error("connection url: {}", request.url)
+                        logger.error("content: {}", result.component1())
+                        logger.error("error: {}", result.error.toString())
+                        throw IOException(result.error.toString())
+                    }
+                }
+            }
+        } while (isRedirect)
     }
 
     logger.debug("saving $url -> $this")
