df061095610745b674824d986949c1d055942cda
NikkyAi <root@nikky.moe>
2018-10-02 15:20:48 +0200

    organize gradle scripts

--- base.gradle.kts
+++ base.gradle.kts
@@ -1,8 +1,8 @@
 val compile by configurations
 val kotlin_version: String by project
-val kotlin_coroutines_version: String by project
+val coroutines_version: String by project
 dependencies {
     compile(kotlin("stdlib", kotlin_version))
     compile(kotlin("stdlib-common", kotlin_version))
-    compile(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = kotlin_coroutines_version)
+    compile(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = coroutines_version)
 }
