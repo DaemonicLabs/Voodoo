9232873b20cdd11ce7190ba8e14b152a29b89c99
NikkyAinikky <root@nikky.moe>
2018-11-11 23:41:03 +0100

    fix arguments not getting applied on tasks

--- plugin/src/main/kotlin/voodoo/VoodooTask.kt
+++ plugin/src/main/kotlin/voodoo/VoodooTask.kt
@@ -7,36 +7,18 @@ import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.options.Option
 
 open class VoodooTask : JavaExec() {
-    @Input
-    val tasks: MutableList<List<String>> = mutableListOf()
+//    @Input
+//    val tasks: MutableList<List<String>> = mutableListOf()
 
     init {
         group = "voodoo"
     }
 
-    // TODO repeatable annotations
-    // values cannot start with --
-
-    @Option(option = "build", description = "Build/Update the modpack")
-    fun setBuild(args: String) {
-        this.tasks += listOf("build", *Commandline.translateCommandline(args))
-    }
-
-    @Option(option = "pack", description = "package the modpack")
-    fun setPack(args: String) {
-        this.tasks += listOf("pack", *Commandline.translateCommandline(args))
-    }
-
-    override fun setArgsString(args: String): JavaExec {
-        this.tasks += Commandline.translateCommandline(args).asList()
-        return this
-    }
-
     @TaskAction
     override fun exec() {
-        println(tasks.map { it.joinToString(", ") })
-        val argsList = tasks.map { it.toList() }.flatMap { it + "-" } // + (args ?: listOf())
-        args = argsList.dropLastWhile { it == "-" }.dropWhile { it == "-" }
+//        println(tasks.map { it.joinToString(", ") })
+//        val argsList = tasks.map { it.toList() }.flatMap { it + "-" } // + (args ?: listOf())
+//        args = argsList.dropLastWhile { it == "-" }.dropWhile { it == "-" }
         println("executing: $args")
 
         super.exec()
