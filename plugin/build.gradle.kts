67fabe506a44596759268b9ef5b3b8ead41fcd8b
NikkyAi <root@nikky.moe>
2019-01-19 09:04:45 +1300

    redo plugin deployment, add failsafe for bootstrap when jenkins is down

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,13 +1,12 @@
 plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
-    `maven-publish`
 }
 
 val major: String by project
 val minor: String by project
 val patch: String by project
-// use "SNAPSHOT" on CI and "dev" locally
+//// use "SNAPSHOT" on CI and "dev" locally
 val versionSuffix = System.getenv("BUILD_NUMBER")?.let { "SNAPSHOT" } ?: "dev"
 version = "$major.$minor.$patch-$versionSuffix"
 
@@ -19,34 +18,3 @@ gradlePlugin {
         }
     }
 }
-
-val sourcesJar by tasks.registering(Jar::class) {
-    classifier = "sources"
-    from(sourceSets["main"].allSource)
-}
-
-// fails due to Jankson
-val javadoc by tasks.getting(Javadoc::class) {}
-val javadocJar by tasks.registering(Jar::class) {
-    classifier = "javadoc"
-    from(javadoc)
-}
-
-publishing {
-    publications {
-        maybeCreate("pluginMaven", MavenPublication::class.java).apply {
-            artifact(sourcesJar.get())
-            artifact(javadocJar.get())
-            groupId = "moe.nikky.voodoo${Env.branch}"
-            version = "$major.$minor.$patch-${Env.versionSuffix}"
-        }
-//        maybeCreate("voodooPoetPluginMarkerMaven", MavenPublication::class.java).apply {
-//            val versionSuffix = System.getenv("BUILD_NUMBER")?.let { "SNAPSHOT" } ?: "dev"
-//            version = "$major.$minor.$patch-$versionSuffix"
-//        }
-    }
-}
-
-rootProject.file("private.gradle")
-    .takeIf { it.exists() }
-    ?.let { apply(from = it) }
