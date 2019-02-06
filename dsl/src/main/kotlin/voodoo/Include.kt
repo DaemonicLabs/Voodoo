b4936c3710be6497251f9e1eca29f4fe766081bf
NikkyAi <root@nikky.moe>
2019-02-07 10:23:19 +1300

    fix Include annotation

--- dsl/src/main/kotlin/voodoo/Include.kt
+++ dsl/src/main/kotlin/voodoo/Include.kt
@@ -1,6 +1,6 @@
 package voodoo
 
-import voodoo.util.asFile
+import voodoo.util.SharedFolders
 import kotlin.script.experimental.api.ResultWithDiagnostics
 import kotlin.script.experimental.api.ScriptCollectedData
 import kotlin.script.experimental.api.ScriptCompilationConfiguration
@@ -11,24 +11,41 @@ import kotlin.script.experimental.api.foundAnnotations
 import kotlin.script.experimental.api.importScripts
 import kotlin.script.experimental.host.toScriptSource
 
+@Repeatable
+@Target(AnnotationTarget.FILE)
 annotation class Include(val filename: String) {
     companion object {
         fun configureIncludes(context: ScriptConfigurationRefinementContext): ResultWithDiagnostics<ScriptCompilationConfiguration> {
             val reports: MutableList<ScriptDiagnostic> = mutableListOf()
-
-            val annotations = context.collectedData?.get(ScriptCollectedData.foundAnnotations)?.takeIf { it.isNotEmpty() }
+//            println("collectedData: '${context.collectedData}'")
+//            context.collectedData?.entries()?.forEach { (key, value) ->
+//                println("collectedData    $key => $value")
+//            }
+            val annotations = context.collectedData?.get(ScriptCollectedData.foundAnnotations).also {
+                //                println("found annotations: '$it'")
+                reports += ScriptDiagnostic("found annotations: '$it'", severity = ScriptDiagnostic.Severity.INFO)
+            }?.takeIf { it.isNotEmpty() }
                 ?: return ScriptCompilationConfiguration(context.compilationConfiguration).asSuccess()
 
-            val rootDir = (System.getProperty("voodoo.rootDir") ?: System.getProperty("user.dir")).asFile.absoluteFile
-            val includeFolder = System.getProperty("voodoo.includeDir")?.asFile ?: rootDir.resolve("include")
+            val includeFolder = SharedFolders.IncludeDir.get()
 
-            return ScriptCompilationConfiguration(context.compilationConfiguration) {
+            val compilationConfiguration = ScriptCompilationConfiguration(context.compilationConfiguration) {
                 annotations.forEach { annotation ->
                     val include = annotation as Include
                     val includedScript = includeFolder.resolve(include.filename)
+                    require(includedScript.exists()) { "$includedScript does not exist" }
                     importScripts.append(includedScript.toScriptSource())
+//                    println("including '$includedScript'")
+                    reports += ScriptDiagnostic(
+                        "including '$includedScript'",
+                        severity = ScriptDiagnostic.Severity.INFO
+                    )
                 }
-            }.asSuccess(reports)
+            }
+//            compilationConfiguration.entries().forEach {
+//                println("onAnnotations    $it")
+//            }
+            return compilationConfiguration.asSuccess(reports)
         }
     }
 }
