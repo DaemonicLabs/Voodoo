299ffe2ce1c663f8b53bc753143777d778bf4c5c
nikky <root@nikky.moe>
2020-04-04 03:33:13 +0200

    improving downloading, less half copied files hopefully

--- util/src/main/kotlin/voodoo/util/DownloadExtension.kt
+++ util/src/main/kotlin/voodoo/util/DownloadExtension.kt
@@ -7,11 +7,11 @@ import io.ktor.client.request.url
 import io.ktor.client.statement.HttpResponse
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
-import io.ktor.http.Url
 import io.ktor.http.isSuccess
 import io.ktor.util.cio.writeChannel
 import io.ktor.utils.io.copyAndClose
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import mu.KLogger
@@ -35,78 +35,77 @@ suspend fun File.download(
     httpClient: HttpClient = client,
     logger: KLogger = Downloader.logger,
     retries: Int = 3
-) {
-    suspend fun retry() = if (retries > 0) {
-        logger.error("attempting to download again in 500 ms")
-        delay(500)
-        download(
-            url = url,
-            cacheDir = cacheDir,
-            validator = validator,
-            httpClient = httpClient,
-            logger = logger,
-            retries = retries - 1
-        )
-    } else {
-        error("failed to download $url")
-    }
+): File = withContext(Dispatchers.IO) {
+    for (retries in (0..retries)) {
+        val cacheFile = cacheDir.resolve(this@download.name)
+        logger.info("downloading $url -> ${this@download}")
+        logger.debug("cacheFile $cacheFile")
+        if (cacheFile.exists() && !cacheFile.isFile) cacheFile.deleteRecursively()
 
-    val cacheFile = cacheDir.resolve(this.name)
-    logger.info("downloading $url -> ${this@download}")
-    logger.debug("cacheFile $cacheFile")
-    if (cacheFile.exists() && !cacheFile.isFile) cacheFile.deleteRecursively()
-
-    logger.debug("validating $cacheFile existence and hash")
-    if (cacheFile.exists() && cacheFile.isFile && validator(cacheFile)) {
-        logger.info("file: $cacheFile exists and can skip download")
-    } else {
-        withContext(Dispatchers.IO) {
-            try {
-                val response = httpClient.request<HttpResponse> {
+        logger.debug("validating $cacheFile existence and hash")
+        if (cacheFile.exists() && cacheFile.isFile && validator(cacheFile)) {
+            logger.info("file: $cacheFile exists and can skip download")
+            this@download.parentFile.mkdirs()
+            cacheFile.copyTo(this@download, overwrite = true)
+        } else {
+            val response = try {
+                httpClient.request<HttpResponse> {
                     url(url)
                     method = HttpMethod.Get
                     header(HttpHeaders.UserAgent, useragent)
                 }
-                if(!response.status.isSuccess()) {
-                    logger.error("invalid statusCode {} from {}", response.status, url.encoded)
-                    logger.error("connection url: ${url}")
-//            logger.error("cUrl: ${request.cUrlString()}")
-                    logger.error("response: $response")
-//                    logger.error("content: {}", result.component1())
-                    logger.error("status: {}", response.status)
-                    retry()
-                }
-                logger.debug("writing $url -> $cacheFile")
-                cacheFile.parentFile.mkdirs()
-                cacheFile.createNewFile()
-                response.content.copyAndClose(cacheFile.writeChannel())
             } catch (e: IOException) {
+                logger.error(e) { "exception in download for url: $url" }
+                delay(1000)
+                continue
+            } catch (e: TimeoutCancellationException) {
                 logger.error(e) { "download timed out for url: $url" }
-                retry()
+                delay(1000)
+                continue
             }
+            if (!response.status.isSuccess()) {
+                logger.error("invalid statusCode {} from {}", response.status, url.encoded)
+                logger.error("connection url: ${url}")
+                logger.error("response: $response")
+                logger.error("status: {}", response.status)
+                delay(1000)
+                continue
+            }
+            logger.debug("writing $url -> $cacheFile")
+            this@download.parentFile.mkdirs()
+            this@download.createNewFile()
+
+            logger.debug("saving $url -> ${this@download}")
+            response.content.copyAndClose(this@download.writeChannel())
         }
-    }
 
-    logger.debug("saving $url -> $this")
-    try {
-        this.parentFile.mkdirs()
-        cacheFile.copyTo(this, overwrite = true)
-    } catch (e: FileAlreadyExistsException) {
-        val fileIsLocked = !this.renameTo(this)
-        logger.error("failed to copy file $cacheFile to $this .. file is locked ? $fileIsLocked")
-        if (!fileIsLocked)
-            cacheFile.copyTo(this, overwrite = true)
-    }
+        logger.debug("saving $url -> $this")
+//        try {
+//            this@download.parentFile.mkdirs()
+//            cacheFile.copyTo(this@download, overwrite = true)
+//        } catch (e: FileAlreadyExistsException) {
+//            val fileIsLocked = !this@download.renameTo(this@download)
+//            logger.error("failed to copy file $cacheFile to $this .. file is locked ? $fileIsLocked")
+//            if (!fileIsLocked)
+//                cacheFile.copyTo(this@download, overwrite = true)
+//        }
 
-    if (!validator(cacheFile)) {
-        cacheFile.delete()
-    }
-    if (!validator(this)) {
-        logger.error("$this did not pass validation")
-    }
+        if (!validator(this@download)) {
+            logger.error("${this@download} did not pass validation")
+            cacheFile.delete()
+            delay(1000)
+            continue
+        }
 
-    logger.debug("done downloading $url -> $this")
+        cacheFile.parentFile.mkdirs()
+        this@download.copyTo(cacheFile, overwrite = true)
+
+        logger.debug("done downloading $url -> ${this@download})")
+        return@withContext cacheFile
+    }
+    error("failed to download $url")
 }
+
 
 /*
 suspend fun File.download(
